6c89cd91987b113c8232e46d7eb8146407a2786b
koukoumpitsa <d.chantzopoulou@gmail.com>
2021-02-16 18:56:16 +0100

    body + productTable + product layout

--- src/Body/Product.js
+++ src/Body/Product.js
@@ -1,18 +1,66 @@
 import styled from "styled-components";
 
 const ProductWrapper = styled.div`
+    height: 80%;
+    // border: 1px solid black;
+    display: grid;
+    grid-template-rows: 3fr 1fr 1fr 1fr 1fr;
+    place-items: start;
+    grid-gap: .3em;
+    margin: 1em;
+`;
+
+const ProductPhoto = styled.div`
 
 `;
+
+const ProductName = styled.p`
+    font-weight: 500;
+    font-size: 20px;
+    font-style: bold;
+`;
+
+const ProductPrice = styled.p`
+    color: #DD6E42;
+    font-weight: 500;
+    font-size: 20px;
+    font-style: bold;
+    letter-spacing: .5px;
+`;
+
+const ProductAvailability = styled.p`
+    font-weight: 400;
+    font-size: 15px;
+    letter-spacing: .5px;
+`;
+
+const ProductAddButton = styled.button`
+    background-color: #F5CB5C;
+    width: 50px;
+    height: 30px;
+    border: none;
+    border-bottom: .5px solid black;
+    cursor: pointer;
+`;
+
 export default function Product(){
     return(
         <ProductWrapper>
-        <div className="item">
-            <p>Image of Product</p>
-            <p>Product name</p>
-            <p>$30</p>
-            <p>Immediatly available</p>
-            <button >+ &#x1F6D2;</button>
-        </div> 
+            <ProductPhoto>
+                <img src="#" alt=""/>
+            </ProductPhoto>
+            <ProductName>
+                Product Name
+            </ProductName>
+            <ProductPrice>
+                $30
+            </ProductPrice>
+            < ProductAvailability>
+            Immediatly available
+            </ProductAvailability>
+            <ProductAddButton>
+            + &#x1F6D2;
+            </ProductAddButton>
         </ProductWrapper>
     );
 }
--- src/Body/ProductTable.js
+++ src/Body/ProductTable.js
@@ -3,11 +3,27 @@ import styled from "styled-components";
 import Product from "./Product";
 
 const ProductTableWrapper = styled.div`
-
+// border: 1px solid black;
+height: 100%;
+width: 100%;
+display: grid;
+grid-gap: 1em;
+grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
 `;
 export default function ProductTable(){
     return(
         <ProductTableWrapper>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
             <Product/>
         </ProductTableWrapper>
     );
