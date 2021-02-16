c777c700a6949d3c9bbf3f4c82641ff39cbba897
koukoumpitsa <d.chantzopoulou@gmail.com>
2021-02-16 19:33:28 +0100

    for review

--- src/Body/ProductTable.js
+++ src/Body/ProductTable.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Product from "./Product";
+import data from "../data";
 
 const ProductTableWrapper = styled.div`
 // border: 1px solid black;
@@ -11,20 +12,9 @@ grid-gap: 1em;
 grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
 `;
 export default function ProductTable(){
-    return(
-        <ProductTableWrapper>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-        </ProductTableWrapper>
-    );
+    return data.map((item,index) => {
+            return (
+                <Product name={item.name} price={item.price} key={index}/>
+            );
+    });
 }
